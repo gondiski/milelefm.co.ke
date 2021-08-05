52be0413c99dfec3d324583c4ad4d5084365e2af
gondiski <enochotieno@rocketmail.com>
2021-08-05 16:29:45 +0300

    Added the resized images to the project to improve loading times

---
+++ resize.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import os, sys
+
+path = "img/"
+dirs = os.listdir( path )
+
+def resize():
+	for item in dirs:
+		if os.path.isfile(path+item):
+			im = Image.open(path+item)
+			f, e = os.path.splitext(path+item)
+			# imResize = im.resize((200,200), Image.ANTIALIAS)
+			# imFinal = imResize.convert('RGB')
+			imFinal = im.convert('RGB')
+			imFinal.save(f + '_resized.jpg', 'JPEG', quality=90, dpi=(150,150))
+
+resize()
